d0758449d61bdac1d1ad9e61c33750184b2a3118
Keith Henderson <khenderson@abcimaging.com>
2024-02-11 18:45:12 -0600

    Got the SPIs to transmit 4 channels of 2000 pixels of data each in parallel.
    Added a LED that flashes every other second as POC
    for a relay driver
    
    Added logic to read values from a PhotoResitor
    to detect amount of light.

--- src/RaspberryPiLighting/LightingExecutor.cs
+++ src/RaspberryPiLighting/LightingExecutor.cs
@@ -1,6 +1,7 @@
 ﻿using Iot.Device.Ws28xx;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using System.Device.Gpio;
 using System.Device.Spi;
 using System.Drawing;
 
@@ -16,7 +17,16 @@ namespace RaspberryPiLighting
         private Color[] _currentFrame;
         private Dictionary<int, int> _ledToPatternMap;
 
-        private SpiDevice _spi;
+        private SpiDevice _spi1;
+        private SpiDevice _spi2;
+        private SpiDevice _spi3;
+        private SpiDevice _spi4;
+
+        const int gpio1Pin = 6; // Header pin 31
+        private GpioController _gpio1;
+
+        const int gpio2Pin = 13; // Header pin 33
+        private double _lightLevel = -1;
 
         public LightingExecutor(IOptionsMonitor<LightingConfiguration> options, IHostApplicationLifetime hostApplicationLifetime)
         {
@@ -26,7 +36,7 @@ namespace RaspberryPiLighting
 
         ~LightingExecutor()
         {
-            _spi.Dispose();
+            _spi1.Dispose();
         }
 
         internal async Task ExecutePatternAsync()
@@ -77,6 +87,8 @@ namespace RaspberryPiLighting
 
             try
             {
+                var lightTask = measureLight(ct2);
+
                 while (!ct2.IsCancellationRequested)
                 {
                     var fpsTask = Task.Delay(TimeSpan.FromMilliseconds(msPF), ct2);
@@ -93,11 +105,13 @@ namespace RaspberryPiLighting
                         var ts = (stopTime - startTime).TotalSeconds;
                         var actualFps = loopCount / ts;
 
-                        Console.Write($"\rActual FPS: {actualFps:F3}     ");
+                        Console.Write($"\rActual FPS: {actualFps:F3}     \tLightLevel: {_lightLevel:F2} uS          ");
                     }
 
                     await fpsTask;
                 }
+
+                await lightTask;
             }
             catch(TaskCanceledException) 
             { 
@@ -117,11 +131,39 @@ namespace RaspberryPiLighting
                 Console.WriteLine("Exiting Pattern Executor");
                 _currentFrame = Enumerable.Range(0, _options.NumberOfLEDs).Select(x => Color.Black).ToArray();
                 displayFrame();
+                _gpio1.Write(gpio1Pin, PinValue.Low);
+
                 Console.WriteLine("LEDs cleared");
                 var ts = (stopTime - startTime).TotalSeconds;
                 var actualFps = loopCount / ts;
                 Console.WriteLine($"Actual FPS was {actualFps} vs target fps of {_options.TargetFps}.");
             }
+        }
+
+        //https://pimylifeup.com/raspberry-pi-light-sensor/
+        private async Task measureLight(CancellationToken ct2)
+        {
+            using var gpio2 = new GpioController();
+            gpio2.OpenPin(gpio2Pin);
+
+            while (!ct2.IsCancellationRequested)
+            {
+                gpio2.SetPinMode(gpio2Pin, PinMode.Output);
+                gpio2.Write(gpio2Pin, PinValue.Low);
+
+                await Task.Delay(TimeSpan.FromMilliseconds(250));
+
+                var startTime = DateTime.UtcNow;
+                gpio2.SetPinMode(gpio2Pin, PinMode.Input);
+
+                while(gpio2.Read(gpio2Pin) == PinValue.Low)
+                { }
+
+                var stopTime = DateTime.UtcNow;
+
+                _lightLevel = (stopTime - startTime).TotalMicroseconds;
+            }
+            gpio2.ClosePin(gpio2Pin);
         }
 
         private void populateLedMap()
@@ -158,9 +200,9 @@ namespace RaspberryPiLighting
 
         private void displayFrame()
         {
-            if (_spi == null)
+            if (_spi1 == null)
             {
-                Console.WriteLine("Creating spi");
+                Console.WriteLine("Creating spi1");
                 // https://pinout.xyz/pinout/spi
                 // https://blog.stabel.family/raspberry-pi-4-multiple-spis-and-the-device-tree/
                 // Bus 0 = 19 (MOSI)      spi0-0cs
@@ -177,19 +219,96 @@ namespace RaspberryPiLighting
                     DataBitLength = 8,
                 };
 
-                _spi = SpiDevice.Create(settings);
+                _spi1 = SpiDevice.Create(settings);
 
-                Console.WriteLine("Created spi");
+                Console.WriteLine("Created spi1");
             }
             
-            var ledStrip = new Ws2812b(_spi, _options.NumberOfLEDs);
+            if (_spi2 == null)
+            {
+                Console.WriteLine("Creating spi2");
+
+                var settings = new SpiConnectionSettings(5)
+                {
+                    ClockFrequency = 2_400_000,
+                    Mode = SpiMode.Mode0,
+                    DataBitLength = 8,
+                };
+
+                _spi2 = SpiDevice.Create(settings);
+
+                Console.WriteLine("Created spi2");
+            }
+
+            if (_spi3 == null)
+            {
+                Console.WriteLine("Creating spi3");
+
+                var settings = new SpiConnectionSettings(3)
+                {
+                    ClockFrequency = 2_400_000,
+                    Mode = SpiMode.Mode0,
+                    DataBitLength = 8,
+                };
+
+                _spi3 = SpiDevice.Create(settings);
+
+                Console.WriteLine("Created spi3");
+            }
+
+            if (_spi4 == null)
+            {
+                Console.WriteLine("Creating spi4");
+
+                var settings = new SpiConnectionSettings(6)
+                {
+                    ClockFrequency = 2_400_000,
+                    Mode = SpiMode.Mode0,
+                    DataBitLength = 8,
+                };
+
+                _spi4 = SpiDevice.Create(settings);
+
+                Console.WriteLine("Created spi4");
+            }
+
+            if (_gpio1 == null)
+            {
+                Console.WriteLine("Creating gpio1");
+
+                _gpio1 = new GpioController();
+                _gpio1.OpenPin(gpio1Pin, PinMode.Output);
+
+                Console.WriteLine("Created gpio1");
+            }
+
+            var ledStrip = new Ws2812b(_spi1, _options.NumberOfLEDs);
+            var ledStrip2 = new Ws2812b(_spi2, _options.NumberOfLEDs);
+            var ledStrip3 = new Ws2812b(_spi3, _options.NumberOfLEDs);
+            var ledStrip4 = new Ws2812b(_spi4, _options.NumberOfLEDs);
+
 
             var img = ledStrip.Image;
+            var img2 = ledStrip2.Image;
+            var img3 = ledStrip3.Image;
+            var img4 = ledStrip4.Image;
             for (int i = 0; i < _options.NumberOfLEDs; i++)
             {
                 img.SetPixel(i, 0, _currentFrame[i]);
+                img2.SetPixel(i, 0, _currentFrame[i]);
+                img3.SetPixel(i, 0, _currentFrame[i]);
+                img4.SetPixel(i, 0, _currentFrame[i]);
             }
-            ledStrip.Update();
+            var t1 = Task.Run(() => ledStrip.Update());
+            var t2 = Task.Run(() => ledStrip2.Update());
+            var t3 = Task.Run(() => ledStrip3.Update());
+            var t4 = Task.Run(() => ledStrip4.Update());
+
+            Task.WhenAll(t1, t2, t3, t4).Wait();
+
+            var v = DateTime.Now.Second % 2 == 0;
+
+            _gpio1.Write(gpio1Pin, v ? PinValue.High : PinValue.Low);
         }
 
         private void populateFrame()
@@ -208,5 +327,6 @@ namespace RaspberryPiLighting
 
             _startingPatternIndex %= _repeatingPattern.Length;
         }
+
     }
 }
